18ae30022af7eeca3412b1aa200aefddaae6dc5d
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-07 15:24:27 +0800

    fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  output: isProd ? 'export' : undefined,
+  output: isProd ? 'out' : undefined,
   // distDir: 'dist',
   reactStrictMode: true,
   images: {

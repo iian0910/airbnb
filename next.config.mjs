c0f1f033b03dacf791a2a7d55e4a992f9d3ecf37
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-07 15:40:55 +0800

    fix config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  output: isProd ? 'out' : undefined,
+  // output: isProd ? 'out' : undefined,
   // distDir: 'dist',
-  reactStrictMode: true,
+  // reactStrictMode: true,
   images: {
     domains: [
       "avatars.githubusercontent.com",
       "lh3.googleusercontent.com"
     ]
   },
-  assetPrefix: isProd
-    ? 'https://iian0910.github.io/airbnb'
-    : undefined
+  // assetPrefix: isProd
+  //   ? 'https://iian0910.github.io/airbnb'
+  //   : undefined
 };
 
 export default nextConfig;

0ac2b42d36f07589523fd0c4151e8de3b849699c
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-16 17:06:08 +0800

    新增刊登項目頁面/刪除刊登

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
+interface HomeProps {
+  searchParams: IListingsParams
+}
 
-export default async function Home() {
+const Home = async ({searchParams}: HomeProps) => {
 
-  const listings = await getListings()
+  const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
 
   if(listings.length === 0) {
@@ -47,3 +50,5 @@ export default async function Home() {
     </ClientOnly>
   );
 }
+
+export default Home;
